90ec697e5af675c80f80a832b9c1948b443b68c4
axxaxya <annanya21sharma@gmail.com>
2022-06-08 18:24:30 -0400

    Initialized the images and changed background

--- frontend/src/components/contact_us/ContactUs.js
+++ frontend/src/components/contact_us/ContactUs.js
@@ -1,10 +1,14 @@
 import React from 'react'; 
 import '../../App.css'; 
+import paints from "../../assets/paints.png"; 
+import ig from "../../assets/ig.png"; 
+import './ContactUs.css';
 
 function ContactUs(){
     return (
-       <div>
-
+       <div className = 'image' >
+           <img className='img1' src={ig}></img>
+           <img className='img2' src={paints}></img>
         </div>
     );
 }
