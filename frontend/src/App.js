9c457ec18ebdf32608a655d596cc7a0fb21ff09b
axxaxya <annanya21sharma@gmail.com>
2022-06-08 17:55:56 -0400

    Added images and initialized the contact us files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import './App.css';
+import ContactUs from './components/contact_us/ContactUs'; 
 
 function App(){
 
-  return(<h1>Hello</h1>)
+  return (
+    <ContactUs></ContactUs>
+  )
 }
 
 export default App
---
+++ frontend/src/components/contact_us/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import '../../App.css'; 
+
+function ContactUs(){
+    return (
+       <div>
+
+        </div>
+    );
+}
+
+export default ContactUs; 
