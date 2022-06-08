e5ab029e834ed4a51a93c064b388efb78168d777
axxaxya <annanya21sharma@gmail.com>
2022-06-08 17:25:06 -0400

    Initialized files for Contact Us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 function App(){
 
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/testing").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
-  return(
-    <div>
-      {(typeof data.testing === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        data.testing.map((testing, i) => (
-          <p key={i}>{testing}</p>
-        ))
-      )}
-    </div>
-  )
+  return(<h1>Hello</h1>)
 }
 
 export default App
