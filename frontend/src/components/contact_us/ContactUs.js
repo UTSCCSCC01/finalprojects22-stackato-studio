3930c743c4823a69cb47223f5dd40b154ce67b9e
axxaxya <annanya21sharma@gmail.com>
2022-06-09 09:53:56 -0400

    Added a file to keep track

--- frontend/src/components/contact_us/ContactUs.js
+++ frontend/src/components/contact_us/ContactUs.js
@@ -7,19 +7,9 @@ import './ContactUs.css';
 function ContactUs(){
     return (
         <div>
-            <h1 style={{textAlign: "center"}} className = "heading"> Contact Us</h1>
-            { <div class="dashed-box"></div> }
-            <img className='img2' src={paints}></img>
-            <div className = "question"> 
-                <h3 style={{textAlign:"Left"}}>Have questions about</h3>
-                <h3 style={{textAlign:"Left"}}>features, pricing, services or</h3>
-                <h3 style={{textAlign:"Left"}}>simply want to reach out?</h3>
-                <h3 style={{textAlign:"Left"}}>Our team would love to help.</h3>
-            </div>
-            
-
-        </div>
-    );
+                 <h1 style={{textAlign: "center"}} className = "heading"> Contact Us</h1>
+                 <div class="dashed-box"></div>
+        </div> 
+    ); 
 }
-
 export default ContactUs; 
