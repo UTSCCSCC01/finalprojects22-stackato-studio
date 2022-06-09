99b4b74e550591d5d1794ec19b0cff2a0fb4dd13
Christopher Nathanael <chrisnathan20@gmail.com>
2022-06-09 00:43:53 -0400

    Revert "Stac 33 home page"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
-import Home from './components/home/Home';
+import React, { useState, useEffect } from 'react'
 
 function App(){
-  return (
-    <>
-      <Home></Home>
-    </>
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/testing").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  return(
+    <div>
+      {(typeof data.testing === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        data.testing.map((testing, i) => (
+          <p key={i}>{testing}</p>
+        ))
+      )}
+    </div>
   )
 }
 
