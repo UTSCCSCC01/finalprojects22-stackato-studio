e14b502fa9ec4fb517b0c5f2e99faaa5dd2f2ae9
theor <theoragavrila@gmail.com>
2022-06-09 16:57:48 -0400

    Edited CSS styling

--- frontend/src/components/about_us/AboutUs.js
+++ frontend/src/components/about_us/AboutUs.js
@@ -15,44 +15,42 @@ function App() {
               </div>*/
   
       <div className="container">
-        <div>
-          <div class="box">
-            <div className="titleContainer">
-              <h1 class="title">
-                <span class="about">About</span>
-                <span class="space"> </span>
-                <span class="a">a</span>
-                <span class="morr">morr</span>
-              </h1>
-            </div>
-            <p class="p1">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              ullamcorper ipsum erat, nec bibendum turpis venenatis eget. Maecenas
-              et rutrum arcu. Cras vehicula mollis lobortis. Curabitur quis velit
-              id augue porta venenatis. Quisque interdum, tellus eu cursus
-              ultricies, sem enim sagittis sapien, mattis commodo arcu nulla at
-              arcu. Ut sit amet rutrum dolor. Nulla tristique enim libero. Aliquam
-              erat volutpat.
-            </p>
-            <p class="p2">
-              Maecenas luctus ultricies nibh eget ultrices. Fusce porttitor
-              fringilla pharetra. Donec et maximus felis. Quisque interdum eros
-              vitae magna aliquam blandit. Nunc eget urna eu neque lacinia
-              pulvinar. Aliquam erat volutpat. Vestibulum non rutrum quam, sed
-              commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
-              sit amet lectus. Nulla posuere tristique purus id fermentum.
-              Maecenas vitae ligula sit amet nisl fringilla aliquet.
-            </p>
-            <div
-              style={{
-                flexDirection: "row",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <button class="click">Connect with Us </button>
-              <img src={peacock} className="img-peacock" />
-            </div>
+        <div class="box">
+          <div className="titleContainer">
+            <h1 class="title">
+              <span class="about">About</span>
+              <span class="space"> </span>
+              <span class="a">a</span>
+              <span class="morr">morr</span>
+            </h1>
+          </div>
+          <p class="p1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            ullamcorper ipsum erat, nec bibendum turpis venenatis eget. Maecenas
+            et rutrum arcu. Cras vehicula mollis lobortis. Curabitur quis velit
+            id augue porta venenatis. Quisque interdum, tellus eu cursus
+            ultricies, sem enim sagittis sapien, mattis commodo arcu nulla at
+            arcu. Ut sit amet rutrum dolor. Nulla tristique enim libero. Aliquam
+            erat volutpat.
+          </p>
+          <p class="p2">
+            Maecenas luctus ultricies nibh eget ultrices. Fusce porttitor
+            fringilla pharetra. Donec et maximus felis. Quisque interdum eros
+            vitae magna aliquam blandit. Nunc eget urna eu neque lacinia
+            pulvinar. Aliquam erat volutpat. Vestibulum non rutrum quam, sed
+            commodo sapien. Donec quam nisi, convallis et faucibus at, dignissim
+            sit amet lectus. Nulla posuere tristique purus id fermentum.
+            Maecenas vitae ligula sit amet nisl fringilla aliquet.
+          </p>
+          <div
+            style={{
+              flexDirection: "row",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <button class="click">Connect with Us </button>
+            <img src={peacock} className="img-peacock" />
           </div>
         </div>
       </div>
