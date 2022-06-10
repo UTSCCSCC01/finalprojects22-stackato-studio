8c119ba04bfed1a89491618da2a9ad2f2e279480
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2022-06-10 00:15:58 -0400

    reconstructed the jsx file, separated everything into divs, added styling to ensure page responsiveness

--- frontend/src/components/contact_us/contact_us_page.jsx
+++ frontend/src/components/contact_us/contact_us_page.jsx
@@ -16,51 +16,42 @@ import location from "../../assets/location.png";
 const contact_us_page = () => {
     return(
         <body>
-            <div className='left'>
-                <div><img src={paints} className="paints" alt="paints" /></div>
-                <div className="box2">
-                    <span> Have questions about <br/>features, pricing, services or <br/>simply want to reach out? <br/>Our team would love to help.</span>
-                    <br/>
-                    <br/>
-                    <span className="info"><Icon icon="ant-design:instagram-filled" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  amorr_ca</span>
-                    <br></br>
-                    <span className="info"><Icon icon="akar-icons:twitter-fill" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  real_amorr_ca</span>
-                    <br></br>
-                    <span className="info"><Icon icon="fluent:mail-16-filled" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  contactus@amorr.ca</span>
-                    <br></br>
-                    <span className="info"><Icon icon="carbon:location-filled" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  1265 Military Trail</span>
-                    <br></br>
+            <div className='container'>
+                <div className='left'>
+                    <div><img src={paints} className="paints" alt="paints" /></div>
+                    <div className="belowImage">
+                        <span> Have questions about <br/>features, pricing, services or <br/>simply want to reach out? <br/>Our team would love to help.</span>
+                        <br/>
+                        <br/>
+                        <span className="info"><Icon icon="ant-design:instagram-filled" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  amorr_ca</span>
+                        <br></br>
+                        <span className="info"><Icon icon="akar-icons:twitter-fill" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  real_amorr_ca</span>
+                        <br></br>
+                        <span className="info"><Icon icon="fluent:mail-16-filled" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  contactus@amorr.ca</span>
+                        <br></br>
+                        <span className="info"><Icon icon="carbon:location-filled" inline={true} style={{ verticalAlign: '-0.2em', fontSize:'28px' }}/>  1265 Military Trail</span>
+                        <br></br>
+                    </div>
                 </div>
-            </div>
-            <div className="right">
-                <div className="heading">Contact Us</div>
-                <div className="box">
-                    <form>
-                        <div className="inputs">
-                            <div className='row'>
+
+                <div className="right">
+                    <div className="heading">Contact Us</div>
+                    
+                    <div className="box">
+                        <form>
+                            <div className="inputs">
                                 <div className="input"><input type="text" placeholder="Full Name"/></div>
-                            </div>
-
-                            <div className='row'>
                                 <div className="input"><input type="text" placeholder="Email Address" required/></div>
-                            </div>
-
-                            <div className='row'>
                                 <div className="input"><input type="text" placeholder="Subject"/></div>
-                            </div>
-
-                            <div className='row'>
                                 <div className="input1" id="message"><textarea className="textbox" rows="10" cols="40" type="text" placeholder="Type your message here..."/></div>
-                            </div>
-
-                            <div className="cardFooter">
                                 <button id="service">Send Message <Icon icon="akar-icons:arrow-right" inline={true} style={{ verticalAlign: '-0.3em', fontSize:'28px' }}/></button>
-                            </div>  
-                        </div>
-                    </form>
+                            </div>
+                        </form>
+                    </div>
+                    
                 </div>
-            </div>
 
+            </div>
         </body>
     );
 }
