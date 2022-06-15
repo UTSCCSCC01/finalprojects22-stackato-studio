ce011ba40bcb545597bfecb2d8e9af79a4118788
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2022-06-14 22:06:46 -0400

    Update App.js
    replaced faulty use of <CoverPage/> with <Home/>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,11 @@ import ContactUs from './components/contact_us/contact_us';
 import CustomerRegistration from './components/customer_registration/customer_registration';
 import ServiceProviderRegistration from './components/service_provider_registration/service_provider_registration';
 import FAQ from './components/FAQ/FAQpage';
-import CoverPage from './components/home/CoverPage/CoverPage';
 import LoginSignupLanding from './components/initial_login_signup_landing/login_signup_landing';
 import InitialLogin from './components/initial-login/initial-login';
 import InitialSignUp from './components/initial-signup/initial-signup';
 import Login from './components/login-page/login-page';
+import Home from './components/home/Home';
 
 import {
   BrowserRouter as Router,
@@ -27,8 +27,8 @@ function App(){
     <Router>
       <Routes>
 
-        <Route exact path='/' element={<CoverPage/>} />
-        <Route exact path='/home' element={<CoverPage/>} />
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/home' element={<Home/>} />
         <Route exact path='/about' element={<AboutUs/>} />
         <Route exact path='/contact' element={<ContactUs/>} />
         <Route exact path='/customer-register' element={<CustomerRegistration/>} />
